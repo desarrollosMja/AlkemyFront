fb0a3db98d3b99e965baa5861592f68868a21812
Desarrollos MJA <desarrollos.mja@gmail.com>
2022-05-15 23:01:19 -0300

    Show login function

--- src/components/login/form/form.js
+++ src/components/login/form/form.js
@@ -1,28 +1,58 @@
 import "./form.css"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import { useState, useRef } from "react"
 
 export default function LoginForm(){
+    let [showLogin, setShowLogin] = useState(true)
+    const formLogin = useRef()
+    const formRegister = useRef()
+
+    function switchLoginRegister(){
+        showLogin == true ? setShowLogin(false) : setShowLogin(true)
+    }
+
     return(
         <div>
-            <Form id="form-login">
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+            {showLogin == true && 
+                <Form id="form-login" ref={formLogin}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="success" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="success" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            }
+            
+            {showLogin == false &&
+                <Form id="create-user" ref={formRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="light" type="submit">
+                        Create user
+                    </Button>
+                </Form>
+            }
 
             <Form.Check 
                 type="switch"
                 id="login-new-account-switch"
+                onChange={switchLoginRegister}
+                variant="light"
             />
             <Form.Label>Login / Create new account</Form.Label>
         </div>
